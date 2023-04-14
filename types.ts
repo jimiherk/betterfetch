f036463e70e81ed449f6a8347270920d211b4d0f
Jimi <jimiherken@icloud.com>
2023-04-14 16:28:12 +0200

    Added some types

---
+++ types.ts
@@ -0,0 +1,11 @@
+export type RequestMethods = "CONNECT" | "DELETE" | "GET" | "HEAD" | "OPTIONS" | "PATCH" | "POST" | "PUT" | "TRACE";
+
+export interface ClientSettings {
+    base?: string,
+    headers?: HeadersInit,
+}
+
+export interface RequestSettings {
+    method: RequestMethods,
+    useBase: boolean,
+}
