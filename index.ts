6ffc93c30ecfcb1c936974f9efe5025c07bd96f9
Jimi <jimiherken@icloud.com>
2023-04-14 16:28:24 +0200

    Started working on Client class

---
+++ index.ts
@@ -0,0 +1,12 @@
+import * as types from './types.ts';
+
+export default class BFC {
+    public settings: types.ClientSettings;
+    constructor(settings: types.ClientSettings) {
+        this.settings = settings;
+    }
+
+    public fetch(url: string, ) {
+
+    }
+}
